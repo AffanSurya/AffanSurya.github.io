7787405e700cd7722ae9f5d4d9eea37a0e740f0a
AffanSurya <affanqwe04@gmail.com>
2024-01-05 13:29:11 +0700

    modified:   11_Database/Latihan/input-aksi.php 	new file:   11_Database/Praktikum/input-perpusKota.php

--- 11_Database/Latihan/input-aksi.php
+++ 11_Database/Latihan/input-aksi.php
@@ -6,6 +6,6 @@ $nama = $_POST['nama'];
 $alamat = $_POST['alamat'];
 $kelas = $_POST['kelas'];
 
-$conn->query("insert into mahasiswa values ('$npm', '$nama', '$alamat, '$kelas')");
+$conn->query("insert into mahasiswa values ('$npm', '$nama', '$alamat', '$kelas')");
 
 header("location:index.php?pesan=input");
